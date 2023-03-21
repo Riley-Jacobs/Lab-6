74c9862e107f5b3a08ca0f7d5312520f2f2ef2d1
hannah-sater <hannah.sater@ufl.edu>
2023-03-21 19:12:22 -0400

    Hannah Sater addition with decode

--- main.py
+++ main.py
@@ -15,6 +15,32 @@ def encode(password):
         encoded_password += str(encoded_digit)
     return encoded_password
 
+# Hannah Sater
+def decode(encoded_password):
+    decoded_password = ''
+    for i in encoded_password:
+        if i == '3':
+            decoded_password = decoded_password + '0'
+        if i == '4':
+            decoded_password = decoded_password + '1'
+        if i == '5':
+            decoded_password = decoded_password + '2'
+        if i == '6':
+            decoded_password = decoded_password + '3'
+        if i == '7':
+            decoded_password = decoded_password + '4'
+        if i == '8':
+            decoded_password = decoded_password + '5'
+        if i == '9':
+            decoded_password = decoded_password + '6'
+        if i == '0':
+            decoded_password = decoded_password + '7'
+        if i == '1':
+            decoded_password = decoded_password + '8'
+        if i == '2':
+            decoded_password = decoded_password + '9'
+    return decoded_password
+
 
 def main():
     encoded_password = None
